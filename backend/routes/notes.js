fc24cdd4783de03dd2e065c313d57ad90133d484
rudrapatirohith <rudrapatirohith1@gmail.com>
2024-03-08 01:12:28 -0600

    Created Updtaing an existing Note Route to update the existing notes using ID of the notes made earlier

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -39,4 +39,33 @@ router.post('/addnote', fetchUser, [
         res.status(500).send("There is some Internal Server Issue"); // we will get to see this in response like in thunderclient response
     }
 })
+
+// ROUTE 3 : UPDATE AN EXISTING NOTE using : PUT "api/notes/updatenote:id" where id is placeholder for notes ID which we got after adding a note
+router.put('/updatenote/:id',fetchUser, async (req,res)=>{
+    const {title,description,tag}=req.body; // Extracts title, description, and tag from the request body
+    // creating a newNote object
+    // Initializes an empty object to hold the new note data.
+    const newNote = {};
+    if(title){newNote.title = title}; // adds title to newNote if provided
+    if(description){newNote.description = description}; // adds description to newNote if provided
+    if(tag){newNote.tag=tag}; // adds tag to newNote if provided
+
+    // find the note to be updated and update it
+    let note=await Note.findById(req.params.id); //Finds the note by ID from the database.
+    if(!note){return res.status(404).send("Not Found")}
+
+    //mongoose stores IDs as objects  and direct comparison between objects and strings would always result in false, even if the IDs are the same.
+    // so i am using string convertion to match
+    //req.user.id id the ID user making the req, obtained fromt the req obj
+    if(note.user.toString() != req.user.id){ //Checks if the note's user ID matches the requesting user's ID.
+        return res.status(401).send("Not Allowed");
+    }
+    // Updates the note in the database with the new data.
+    //The $set operator is used to specify the fields to update. It updates the document with the new values contained in the newNote object. 
+    //The {new:true} option tells Mongoose to return the updated document rather than the original
+    note=await Note.findByIdAndUpdate(req.params.id,{$set: newNote}, {new:true}) 
+    res.json(note);    
+}
+)
+
 module.exports = router
